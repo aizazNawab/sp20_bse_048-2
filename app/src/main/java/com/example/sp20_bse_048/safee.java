5c4c1b21c7f2bd2991bef3d7bd903a0168284eab
aizaz77777 <abbas8901741@gmail.com>
2022-04-24 21:39:20 +0500

    assignment 02 final

---
+++ app/src/main/java/com/example/sp20_bse_048/safee.java
@@ -0,0 +1,15 @@
+package com.example.sp20_bse_048;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class safee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safee);
+    }
+}
